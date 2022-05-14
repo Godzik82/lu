683c4f5011a936d0747320300953e8fb064873a6
Godzik82 <godzik82@mail.ru>
2022-05-14 09:19:40 +0400

    Add table users

--- src/main/java/lu/App.java
+++ src/main/java/lu/App.java
@@ -10,21 +10,22 @@ public final class App {
     public static void main(String[] args) {
         
         try {
-            
             Class.forName("org.sqlite.JDBC");
-            System.out.println("Ok");
-            Connection con = DriverManager.getConnection("jdbc:sqlite:./my.db");
-            System.out.println("Ok");
-            // DriverManager.registerDriver(new JDBC());
         } catch (Exception e) {
             e.printStackTrace();;
         }
-        // DriverManager.registerDriver(new JDBC());
-        // try () {
-        //     System.out.println("Ok");
-        // } catch (SQLException e1) {
-        //     e1.printStackTrace();
-        // }
+            
+        try (Connection con = DriverManager.getConnection("jdbc:sqlite:./my.db")) {
+            
+            try (Statement statement = con.createStatement()){
+                String query = "ALTER TABLE users ADD surname TXT;";
+                statement.executeUpdate(query);
+                System.out.println("OK");
+            } 
+            
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }   
 }
 
